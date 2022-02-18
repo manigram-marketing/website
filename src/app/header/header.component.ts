dae6f3dcefb67c1fa075a55398388e2999dd2be5
Jason <jason.bysaeth@outlook.de>
2022-02-18 11:04:43 +0100

    Changes

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -10,8 +10,23 @@ export class HeaderComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-  }
-  onScroll(): void {
-    console.log('test');
+    // tslint:disable-next-line:only-arrow-functions
+    document.addEventListener('scroll', function(): void {
+      const position: number = window.scrollY;
+      if (position !== 0) {
+        document.getElementById('header').style.color = 'black';
+        document.getElementById('header').style.backgroundColor = '#fff';
+        document.getElementById('header').style.position = 'fixed';
+        document.getElementById('header').style.top = '0px';
+        document.getElementById('header').style.zIndex = '10';
+        document.getElementById('logo').style.width = '50px';
+        document.getElementById('navigation-badge').style.color = 'black';
+      } else {
+        document.getElementById('header').style.color = 'white';
+        document.getElementById('header').style.backgroundColor = 'rgba(128, 128, 128, 0.84)';
+        document.getElementById('logo').style.width = '80px';
+        document.getElementById('navigation-badge').style.color = '#fff';
+      }
+    });
   }
 }
